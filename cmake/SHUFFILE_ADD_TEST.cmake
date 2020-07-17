d10b2a5c295dfabd0433a9b399c2f25a20894fc6
Gregory A. Kosinovsky <kosinov@quartz188.llnl.gov>
2020-07-16 17:09:29 -0700

    added SHUFFILE_ADD_TEST.cmake

---
+++ cmake/SHUFFILE_ADD_TEST.cmake
@@ -0,0 +1,17 @@
+function(shuffile_add_test name args outputs)
+
+  # job launcher
+  if(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
+    # Assume mpirun if no resource manager in place
+    set(test_param_s mpirun -np 1)
+    set(test_param_p mpirun -np ${parallel_nodes})
+  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
+    set(test_param jsrun -nnodes 3 -n 6)
+  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
+    set(test_param_s srun -N 3 -n 6)
+  endif()
+
+  # Tests
+  add_test(NAME serial_${name}_start COMMAND ${test_param} ./${name} ${args} )
+
+endfunction(veloc_add_test)
