9c24996ff69532689e8d6904b7783705b3c05629
Gregory A. Kosinovsky <kosinov@quartz188.llnl.gov>
2020-07-22 13:31:26 -0700

    corrected typo

--- cmake/SHUFFILE_ADD_TEST.cmake
+++ cmake/SHUFFILE_ADD_TEST.cmake
@@ -1,14 +1,10 @@
 function(SHUFFILE_ADD_TEST name args outputs)
 
   # job launcher
-  if(${VELOC_RESOURCE_MANAGER} STREQUAL "NONE")
-    # Assume mpirun if no resource manager in place
-    set(test_param_s mpirun -np 1)
-    set(test_param_p mpirun -np ${parallel_nodes})
-  elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
+  if(${VELOC_RESOURCE_MANAGER} STREQUAL "LSF")
     set(test_param jsrun -nnodes 3 -n 6)
   elseif(${VELOC_RESOURCE_MANAGER} STREQUAL "SLURM")
-    set(test_param_s srun -N 3 -n 6)
+    set(test_param srun -N 3 -n 6)
   endif()
 
   # Tests
