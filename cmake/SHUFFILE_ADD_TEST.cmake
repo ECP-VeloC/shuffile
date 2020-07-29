6cfb7b7f3a70c556495287a9cb6f37b3051ecc23
Gregory A. Kosinovsky <kosinov@quartz1538.llnl.gov>
2020-07-29 09:33:25 -0700

    Corrected test name in .cmake

--- cmake/SHUFFILE_ADD_TEST.cmake
+++ cmake/SHUFFILE_ADD_TEST.cmake
@@ -8,6 +8,6 @@ function(SHUFFILE_ADD_TEST name args outputs)
   endif()
 
   # Tests
-  add_test(NAME serial_${name}_start COMMAND ${test_param} ./${name} ${args} )
+  add_test(NAME ${name} COMMAND ${test_param} ./${name} ${args} )
 
 endfunction(SHUFFILE_ADD_TEST)
